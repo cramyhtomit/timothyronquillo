70be325b494cc729b5cfd03af97aa74181a5941d
projectwishlist <project.wishlist.2023@gmail.com>
2023-03-16 18:27:50 +0300

    Fixed!

--- scripts.js
+++ scripts.js
@@ -7,7 +7,6 @@ window.addEventListener('scroll', function(){
         this.document.getElementById('arrow').style.opacity = "1";
         this.document.getElementById('arrow').style.transform = "scale(1)";
     }
-
     if (scrolled >= 900){
         this.document.getElementById('imAnIntrovert').style.opacity = "1";
     } 
@@ -15,42 +14,22 @@ window.addEventListener('scroll', function(){
 
 
 
-// var listItem = document.querySelectorAll('li');
-// for ( i = 0; i < listItem.length; i++){
+var listItem = document.querySelectorAll('li');
+for ( i = 0; i < listItem.length; i++){
 
-//     var colorValue = window.getComputedStyle(listItem[i]).getPropertyValue('color');
+    listItem[i].addEventListener('mouseover', function() {
 
-//     // if (colorValue == "rgb(58, 152, 185)"){
-//     //     listItem[i].addEventListener('mouseover', function() {
-//     //         this.style.color = "rgb(242, 76, 76)";
-//     //     });
+        var colorValue = window.getComputedStyle(this).getPropertyValue('color');
+        if (colorValue == "rgb(255, 255, 255)"){
+            this.style.color = "rgb(242, 76, 76)";
+        } else if (colorValue == "rgb(58, 152, 185)"){
+            this.style.color = "rgb(255, 255, 255)";
+        } else {
+            this.style.color = "rgb(58, 152, 185)";
+        }
 
-//     // }
+    });
 
-//     // if (colorValue == "rgb(255, 255, 255)"){
-//     //     listItem[i].addEventListener('mouseover', function() {
-//     //         this.style.color = "rgb(58, 152, 185)";
-//     //     })
-//     // }
-
-//     listItem[i].addEventListener('mouseover', function() {
-//         if (colorValue == "rgb(58, 152, 185)"){
-//             this.style.color = "rgb(242, 76, 76)";
-//         } else if (colorValue == "rgb(242, 76, 76)"){
-//             this.style.color = "rgb(255, 255, 255)";
-//         } else {
-//             this.style.color = "rgb(58, 152, 185)";
-//         }
-//     });
+}
 
 
-    
-// }
-
-
-
-window.addEventListener('mouseover', function() {
-
-    console.log(window.getComputedStyle(this).getPropertyValue('color'));
-
-});
