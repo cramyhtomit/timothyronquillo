f88227bedbf40e9d9c1055cff1968e6eff96ba61
projectwishlist <project.wishlist.2023@gmail.com>
2023-03-16 09:10:33 +0300

    about me added im an introvert

--- scripts.js
+++ scripts.js
@@ -2,7 +2,13 @@ window.addEventListener('scroll', function(){
     const scrolled = window.scrollY;
     if (scrolled >= 100){
         this.document.getElementById('arrow').style.opacity = "0";
+        this.document.getElementById('arrow').style.transform = "scale(10)";
     }else{
         this.document.getElementById('arrow').style.opacity = "1";
+        this.document.getElementById('arrow').style.transform = "scale(1)";
     }
+
+    if (scrolled >= 900){
+        this.document.getElementById('imAnIntrovert').style.opacity = "1";
+    } 
 });
