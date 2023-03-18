c790ac48b440e9765d8e6c4d41ee79e150c7c9f3
cramyhtomit <87903110+cramyhtomit@users.noreply.github.com>
2023-03-18 19:15:12 +0300

    body overflow

--- scripts.js
+++ scripts.js
@@ -1,23 +1,7 @@
-// window.addEventListener('scroll', function(){
-//     const scrolled = window.scrollY;
-//     // if (scrolled >= 10){
-//     //     // this.document.getElementById('panda').style.position = 'fixed';
-//     //     // document.getElementById('panda').style.transform = 'translateY(1000px)';
-//     //     // document.getElementById('panda').style.opacity = 0;
-//     //     // document.getElementById('panda').style.transition = 'transform 1s ease';
-//     // }
-
-//     if (scrolled >= 900) {
-//         this.document.getElementById('imAnIntrovert').style.opacity = 1;
-//     }
-// });
-
 
 document.getElementById('pandaDown').addEventListener('mouseenter', function() {
     this.style.transform = 'translateY(100px)';
-    // this.style.display = 'none';
     this.style.transition = 'transform .5s ease';
-    // this.style.cursor = '-webkit-grab, grab';
 
     document.getElementById('me').style.opacity = 0;
     document.getElementById('about').style.opacity = 1;
